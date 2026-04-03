ef5c4732917b7a7865a7eb466cbc10c166bfd12e
Nidhi Panwar <nidhi.panwar@ibm.com>
2026-04-03 20:36:21 +0530

    Adding more programs

---
+++ Functions.py
@@ -0,0 +1,41 @@
+# write a function to calculate and return the square of a number
+
+number = int(input("Enter a number:"))
+def square_of_num(num):
+    return num ** 2
+
+result = square_of_num(number)
+print("The square of given number is:",result)
+
+# create a function that takes two numbers as parameters and returns their sum
+
+num1 = int(input('Enter first number:'))
+num2 = int(input('Enter second number:'))
+
+def sum_of_num(a,b):
+    return a+b
+
+result = sum_of_num(num1,num2)
+print("The sum of two numbers are:",result)
+
+# write a function multiply that multiply two numbers but can also accepts and multiply strings
+
+
+def multiply(a,b):
+    return a * b
+
+print('The multiplication is:',multiply(3,4))
+print('The multiplication is:',multiply('hello',4))
+print('The multiplication is:',multiply(3,'bye'))
+
+# create a function that returns both the area and circumference of a circle given its radius
+
+radius = float(input("Enter radius:"))
+
+def circle_stats(r):
+    area= 3.14 * r * r
+    circumference = 2 * 3.14 * r
+    return area,circumference
+a , c = circle_stats(radius)
+print('Area of circle is:',a)
+print('Circumference of circle is:',c) 
--- Loops.py
+++ Loops.py
@@ -29,8 +29,59 @@ for i in range(1, 11):
 
     #reverse a string using for loop
 string = input("Enter a string:")
-reverse_string=''
+reverse_string = ''
 
 for char in string:
-    reverse_string=char + reverse_string
+    reverse_string = char + reverse_string
 print(reverse_string)
+
+# given a string find first non-repeated character in it.
+
+string = input("Enter a string:")
+repeated_char = ''
+
+for char in string:
+    if string.count(char) == 1:
+        print('The first non-repeated character is:',char)
+        break
+# compute the factorial of a number using while loop
+num1 = int(input("Enter number:"))
+factorial = 1
+
+while num1 > 0:
+    factorial = factorial * num1
+    num1 = num1 - 1
+
+print('The factorial of given number is:',factorial)    
+
+# keep asking the user for input until they enter a number between 1 and 10
+num = int(input("Enter a number between 1 to 10:"))
+while True:
+    if  1<= num <=10:
+        print('The entered number is',num)
+        break
+    else:
+        print("Enter a valid number")   
+        break
+# check if number is prime or not
+num = int(input("Enter a number :"))
+is_prime = True
+if num > 1:
+    for i in range(2, num):
+        if (num % i) == 0:
+            is_prime = False
+            break
+print(is_prime,)        
+
+# check if elements of a list are unique. if duplicates are found exit the loop
+# and print the duplicate items = ["apple", "banana", "orange", "apple", "grape"]
+
+items = ["apple", "banana", "orange", "apple", "grape"]
+
+unique_item = set()
+
+for item in items:
+    if item in unique_item:
+        print("Duplicate item:",item)
+        break
+    unique_item.add(item)
